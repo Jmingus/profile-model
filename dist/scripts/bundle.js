5e22a6f1188aada5a87120648437a619d9821ab7
Jmingus <jmingus@austin.rr.com>
2015-09-22 13:54:11 -0500

    Initial Commit done with normal mode

---
+++ dist/scripts/bundle.js
@@ -0,0 +1,49 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var user = new UserModel();
+var App = Backbone.Router.extend({
+	routes: {
+		'home': 'homePage',
+		'': 'homePage',
+		'edit': 'editForms'
+	},
+	profile: function profile() {
+		$('.page').hide();
+		$('#profile').show();
+	},
+	editForms: function editForms() {
+		$('.page').hide();
+		$('#edit').show();
+		$('#name').val(user.get('name'));
+		$('#inputEmail3').val(user.get('email'));
+		$('#role').val(user.get('role'));
+	},
+	homePage: function homePage() {
+		$('.page').hide();
+		$('#profile').show();
+	}
+});
+
+var app = new App();
+Backbone.history.start();
+
+$('.form-horizontal').submit(function () {
+	user.set({
+		name: $('#name').val(),
+		email: $('#inputEmail3').val(),
+		role: $('#role').val(),
+		password: $('#inputPassword3').val()
+	});
+});
+
+user.on('change', function () {
+	$('.username').html(user.get('name'));
+	$('.dropdown-toggle.username').html(user.get('name') + '<span class="caret"></span>');
+	$('.profile-usertitle-job').text(user.get('role'));
+});
+
+},{}]},{},[1])
+
+
+//# sourceMappingURL=bundle.js.map
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,41 @@
 var user = new UserModel();
 var App = Backbone.Router.extend({
 	routes: {
-		'': 'profile',
-		'edit': 'edit'
+		'home': 'homePage',
+		'': 'homePage',
+		'edit': 'editForms'
 	},
 	profile: function() {
 		$('.page').hide();
 		$('#profile').show();
 	},
-	edit: function() {
+	editForms: function() {
 		$('.page').hide();
 		$('#edit').show();
+		$('#name').val(user.get('name'));
+		$('#inputEmail3').val(user.get('email'));
+		$('#role').val(user.get('role'));
+	},
+	homePage: function(){
+		$('.page').hide()
+		$('#profile').show()
 	}
 });
 
 var app = new App();
 Backbone.history.start();
+
+$('.form-horizontal').submit(function(){
+	user.set({
+		name: $('#name').val(),
+		email: $('#inputEmail3').val(),
+		role: $('#role').val(),
+		password: $('#inputPassword3').val(),
+	})
+});
+
+user.on('change', function(){
+		$('.username').html(user.get('name'));
+		$('.dropdown-toggle.username').html(user.get('name')+'<span class="caret"></span>')
+		$('.profile-usertitle-job').text(user.get('role'));
+})
